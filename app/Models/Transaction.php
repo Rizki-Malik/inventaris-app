3a6e9341bbca9c9dfec4bc0edbe56d709c34f112
Ridat Maulana <ridatmaulana28@gmail.com>
2025-05-06 13:38:05 +0700

    Add: Transaction

--- app/Models/Transaction.php
+++ app/Models/Transaction.php
@@ -3,8 +3,36 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
 
 class Transaction extends Model
 {
-    //
+    use HasUuids;
+
+    protected $fillable = [
+        'item_id',
+        'quantity',
+        'transaction_type',
+        'transaction_date',
+        'notes',
+        'user_id'
+    ];
+
+    protected $table = 'transactions';
+    protected $primaryKey = 'id';
+
+    public function getRouteKeyName()
+    {
+        return 'id';
+    }
+
+    public function item()
+    {
+        return $this->belongsTo(Item::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
