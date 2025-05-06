9eb8453f73d076932c64117add4d8b7fd9ee2bc2
Ridat Maulana <ridatmaulana28@gmail.com>
2025-05-06 09:57:48 +0700

    Categories BackEnd

--- app/Models/categories.php
+++ app/Models/categories.php
@@ -3,8 +3,24 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
 
 class categories extends Model
 {
-    //
+    use HasUuids;
+
+    protected $fillable = [
+        'name',
+        'description',
+    ];
+
+    protected $casts = [
+        'id' => 'uuid',
+    ];
+    protected $table = 'categories';
+    protected $primaryKey = 'id';
+    public function getRouteKeyName()
+    {
+        return 'id';
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -13,5 +13,10 @@ Route::middleware(['auth', 'verified'])->group(function () {
     })->name('dashboard');
 });
 
+Route::middleware(['auth', 'verified'])->group(function () {
+    Route::resource('categories', 'App\Http\Controllers\CategoriesController')->except(['show']);
+    Route::get('categories/{category:uuid}', 'App\Http\Controllers\CategoriesController@show')->name('categories.show');
+});
+
 require __DIR__.'/settings.php';
 require __DIR__.'/auth.php';
